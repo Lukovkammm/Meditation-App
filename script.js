850f4911510a32f56a74aaac748ac7a915f302f2
Marina <artyusmarina@yandex.ru>
2021-01-22 14:01:13 +0300

    fix: change layout

--- script.js
+++ script.js
@@ -1,16 +1,52 @@
 const song = document.querySelector('.song');
 const play = document.querySelector('.play');
 const video = document.querySelector('video');
-const playingTimeButtons = document.querySelectorAll('.time');
-const weather = document.querySelectorAll('.weather button');
+const weatherButtons = document.querySelector('.weather-buttons');
+const timerButtons = document.querySelector('.time-buttons')
 const repeatButton = document.querySelector('.replay');
-
+const outline = document.querySelector('.moving-outline circle');
+const outlineLength = outline.getTotalLength();
+outline.style.strokeDasharray = outlineLength;
+outline.style.strokeDashoffset = outlineLength;
 
 let boolPlayingTimer = false;
 
+const timerConfig = [{
+    time: 120,
+    text: '2 minutes'
+}, {
+    time: 300,
+    text: '5 minutes'
+}, {
+    time: 600,
+    text: '10 minutes'
+}];
+
+const videoConfig = [{
+    name: 'rain',
+    sound: './sounds/rain.mp3',
+    video: './video/Rain - 17013.mp4'
+}, {
+    name: 'beach',
+    sound: './sounds/beach.mp3',
+    video: './video/beach- 31416.mp4'
+}];
+
 window.onload = function () {
-    setInterval(showTime, 500);
+    setInterval(showTime, 1000);
 }
+
+const timerContent = timerConfig.reduce(((acc, item) => {
+    return acc + `
+    <button class="time" data-time="${item.time}">${item.text}</button>`
+}), '');
+timerButtons.innerHTML = timerContent;
+
+const videoContent = videoConfig.reduce(((acc, item) => {
+    return acc + `
+    <button class="${item.name}" data-sound="${item.sound}" data-video="${item.video}"></button>`
+}), '');
+weatherButtons.innerHTML = videoContent;
 
 function showTime() {
     if (!boolPlayingTimer) {
@@ -34,11 +70,6 @@ function playSong() {
         play.style.backgroundImage = 'url("img/play.svg")';
     }
 }
-
-const outline = document.querySelector('.moving-outline circle');
-const outlineLength = outline.getTotalLength();
-outline.style.strokeDasharray = outlineLength;
-outline.style.strokeDashoffset = outlineLength;
 
 function progressCircle(durationPlaying) {
     song.ontimeupdate = () => {
@@ -70,16 +101,16 @@ function playingTime(e) {
 const timer = document.querySelector('.timer');
 
 function animateTimer(minutes, seconds) {
-    if(seconds > 10) {
+    if(seconds >= 10) {
         timer.textContent = `${minutes}:${seconds}`;
     } else {
         timer.textContent = `${minutes}:0${seconds}`;
     }
 }
 
-function pickWeather() {
-    song.src = this.dataset.sound;
-    video.src = this.dataset.video;
+function pickWeather(e) {
+    song.src = e.target.dataset.sound;
+    video.src = e.target.dataset.video;
     playSong();
 }
 
@@ -89,6 +120,17 @@ function repeat() {
 }
 
 play.addEventListener('click', playSong);
-playingTimeButtons.forEach(time => time.addEventListener('click', playingTime));
-weather.forEach(button => button.addEventListener('click', pickWeather));
+timerButtons.addEventListener('click', (e) => {
+    const activeButton = e.target.children;
+    if (activeButton) {
+        playingTime(e);
+    }
+});
+
+weatherButtons.addEventListener('click', (e) => {
+    const activeButton = e.target.children;
+    if (activeButton) {
+        pickWeather(e);
+    }
+});
 repeatButton.addEventListener('click', repeat);
